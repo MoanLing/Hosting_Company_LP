bd1e9b39a16e18f2e4c5c703f996be531e901879
Moan <mewmewmewcj@gmail.com>
2023-03-26 20:24:31 +0300

    Added all styles and functions to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
     source_folder = 'src',
     build_folder = 'dist',
+    testing_folder = 'testing',
     {src, dest, parallel, series} = require('gulp'),
     sass = require('gulp-sass')(require('sass')),
     browserSync = require('browser-sync').create(),
@@ -12,6 +13,7 @@ let path = {
     src: {
         html: source_folder + '/*.html',
         scss: source_folder + '/scss/*.scss',
+        css: source_folder + '/css/*.css',
         img: source_folder + '/img/**/*.{png,svg,jpeg,jpg,jiff,gif.webp,ico}',
         js: source_folder + '/js/*.js'
     },
@@ -21,39 +23,75 @@ let path = {
         css: build_folder + '/css',
         img: build_folder + '/img',
         js: build_folder + '/js'
+    },
+
+    testing: {
+        html: testing_folder + '/',
+        css: testing_folder + '/css',
+        img: testing_folder + '/img',
+        js: testing_folder + '/js'
     }
 }
 
-function html() {
+function htmlBuild() {
     return src(path.src.html)
         .pipe(dest(path.build.html))
         .pipe(browserSync.stream())
 }
-function scssConv() {
+
+function htmlTesting() {
+    return src(path.src.html)
+        .pipe(dest(path.testing.html))
+        .pipe(browserSync.stream())
+}
+
+function scssConvBuild() {
     return src(path.src.scss)
         .pipe(concat('style.min.scss'))
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(rename('style.min.css'))
+        .pipe(concat('style.min.css'))
         .pipe(dest(path.build.css))
         .pipe(browserSync.stream())
 }
 
-function jssConv() {
+function scssConvTesting() {
+    return src(path.src.scss)
+        .pipe(concat('style.scss'))
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(concat('style.css'))
+        .pipe(dest(testing_folder + '/css'))
+        .pipe(browserSync.stream())
+}
+
+function jssConvBuild() {
     return src(path.src.js)
         .pipe(concat('script.js'))
         .pipe(uglify())
-        .pipe(rename('script.min.js'))
+        .pipe(concat('script.min.js'))
         .pipe(dest(path.build.js))
         .pipe(browserSync.stream())
 }
 
-function imgConv() {
+function jssConvTesting() {
+    return src(path.src.js)
+        .pipe(concat('script.js'))
+        .pipe(dest(path.testing.js))
+        .pipe(browserSync.stream())
+}
+
+function imgConvBuild() {
     return src(path.src.img)
         .pipe(dest(path.build.img))
         .pipe(browserSync.stream())
 }
 
-function bs() {
+function imgConvTesting() {
+    return src(path.src.img)
+        .pipe(dest(path.testing.img))
+        .pipe(browserSync.stream())
+}
+
+function bsBuild() {
     browserSync.init({
         server: {
             baseDir: build_folder
@@ -63,13 +101,32 @@ function bs() {
     })
 }
 
-function watching() {
-    gulp.watch(path.src.html).on('change', browserSync.reload)
-    gulp.watch(path.src.scss, scssConv)
-    gulp.watch(path.src.js, jssConv)
-    gulp.watch(path.src.img, imgConv)
+function bsTesting() {
+    browserSync.init({
+        server: {
+            baseDir: testing_folder
+        },
+        port: 3000,
+        notify: false
+    })
 }
 
-exports.build = series(html, scssConv, jssConv, imgConv, parallel(bs, watching))
+function watchingBuild() {
+    gulp.watch(path.src.html, htmlBuild).on('change', browserSync.reload)
+    gulp.watch(path.src.scss, scssConvBuild)
+    gulp.watch(path.src.js, jssConvBuild)
+    gulp.watch(path.src.img, imgConvBuild)
+}
+
+function watchingTesting() {
+    gulp.watch(path.src.html, htmlTesting).on('change', browserSync.reload)
+    gulp.watch(path.src.scss, scssConvTesting)
+    gulp.watch(path.src.js, jssConvTesting)
+    gulp.watch(path.src.img, imgConvTesting)
+}
+
+exports.build = series(htmlBuild, scssConvBuild, jssConvBuild, imgConvBuild, parallel(bsBuild, watchingBuild));
+
+exports.testing = series(htmlTesting, scssConvTesting, jssConvTesting, imgConvTesting, parallel(bsTesting, watchingTesting));
 
 
